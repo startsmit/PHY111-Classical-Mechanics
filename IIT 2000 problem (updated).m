bd1a684daedde9db2db6738055217522f424af69
Hirmay Sandesara <56473003+Hirmay@users.noreply.github.com>
2021-02-08 18:11:24 +0530

    Create IIT 2000 problem (updated).m

---
+++ IIT 2000 problem (updated).m
@@ -0,0 +1,2 @@
+### Kinematics problem of IIT (2000)
+#### This is an interesting problem of kinematics from IIT (2000), it is slightly different from normal problems that we come across. Therefore I thought doing a video explaining and solving this problem will certainly help people grasp kinematics properly. I have also added a small animation for better understanding the problem.
